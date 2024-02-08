419834ecdee8c892770558d1047c542c9b837f6e
Raphael Tavares <77020757+phaelstavares@users.noreply.github.com>
2024-02-08 14:26:21 -0300

    operador de comparação >=, if, else

--- scripts.js
+++ scripts.js
@@ -3,7 +3,15 @@ function buttonClick() {
     const max = Math.floor(document.querySelector(".input-e").value)
     // '.value' é para somente mostrar o valor.
 
-    const result = Math.floor(Math.random() * (max - min +1)) + min;
+
+    // Operador de Comparação >= (maior ou igual que)
+    if(min >= max){
+        alert("O primeiro valor tem q ser MENOR que o segundo.")
+
+        // se o 'min' for MENOR que o 'max', o código roda o else
+    } else {
+        const result = Math.floor(Math.random() * (max - min +1)) + min;
     
-    alert(result)
+        alert(result)
+    }
 }
